5977347eda8147c36d12f111d9072fb10f66012f
Zibri <Zibri@users.noreply.github.com>
2019-03-21 13:39:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -o rand2.o -c -Ofast -flto -pipe -fPIC -fno-plt -fstack-protector-strong -D_GNU_SOURCE rand2.c
-gcc -o rand2 -Wl,-flto -lm rand2.o
+gcc -o rand2.o -funroll-loops -mtune=native -march=corei7 -msse4.2 -c -O3 -flto -pipe -fPIC -fno-plt -fstack-protector-strong -D_GNU_SOURCE rand2.c
+gcc -o rand2 -funroll-loops -mtune=native -march=corei7 -msse4.2 -Wl,-flto -lm rand2.o
 rm rand2.o
 strip -s rand2
