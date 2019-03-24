90a5fcc5bc13bcded46f7e91f16151c943a067d4
Zibri <Zibri@users.noreply.github.com>
2019-03-24 14:54:30 +0200

    Update rand2.c

--- rand2.c
+++ rand2.c
@@ -9,7 +9,7 @@ Audio white noise
 ./rand2 | ffplay -f u16be -ar 48000 -ac 1 -
 Audio + video color white noise
 ./rand2 | ffmpeg -f u16be -ar 48000 -ac 1 -i - -f rawvideo -pixel_format yuv420p -video_size 1920x1080 -framerate 60 -i - -c:a ac3 -vcodec copy -f avi - |ffplay -fs -volume 10 -
-on my pc the bandwidth is about 1.95 GB/sec :D
+on my pc the bandwidth is about 6.20 GB/sec :D
 *****************************************************************************/  
 #include <stdio.h>
 #include <stdlib.h>
