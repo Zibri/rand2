d2c2f7aa7b3ea2d11b9ff4d08beaf81c62ac66d8
Zibri <Zibri@users.noreply.github.com>
2019-03-30 14:41:35 +0200

    Update build.sh
    Added SLOWRND flag (0/1)
    Added ALGO selection (0/1)

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-gcc -Wall -o rand2.o -funroll-loops -mtune=native -march=corei7 -msse4.2 -c -O3 -flto -pipe -fPIC -fno-plt -fstack-protector-strong -D_GNU_SOURCE rand2.c
-gcc -o rand2 -funroll-loops -mtune=native -march=corei7 -msse4.2 -Wl,-flto -lm rand2.o
+[ -z "$1" ] && SR=0 || SR=$1
+[ -z "$2" ] && A=0 || A=$2
+gcc -Wall -DSLOWRND=$SR -DALGO=$A -pthread -Wall -o rand2.o -funroll-loops -mtune=native -march=corei7 -msse4.2 -c -O3 -flto -pipe -fPIC -fstack-protector-strong -D_GNU_SOURCE rand2.c
+gcc -pthread -o rand2 -funroll-loops -mtune=native -march=corei7 -msse4.2 -Wl,-flto -lm rand2.o
 rm rand2.o
 strip -s rand2
