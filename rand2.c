530b32c4510df95b741a9f3e98c5d2bf9a17ef57
Zibri <Zibri@users.noreply.github.com>
2019-03-31 11:44:36 +0200

    Some more optimizations
    Raised process and io priority to max (if run by root).
    Pipe size increased.

--- rand2.c
+++ rand2.c
@@ -11,7 +11,7 @@ Audio + video color white noise
 ./rand2 | ffmpeg -f u16be -ar 48000 -ac 1 -i - -f rawvideo -pixel_format yuv420p -video_size 1920x1080 -framerate 60 -i - -c:a ac3 -vcodec copy -f avi - |ffplay -fs -volume 10 -
 Speed test
 sudo nice -n -20 ./rand2 | pv -ptebaSs 800G >/dev/null
-on my pc the bandwidth is about 7.41 GB/sec :D
+on my pc the bandwidth is about 7.86 GB/sec :D
 *****************************************************************************/
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -23,6 +23,9 @@ on my pc the bandwidth is about 7.41 GB/sec :D
 #include <pthread.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <sys/syscall.h>
 
 #ifndef SLOWRND
 #define SLOWRND 0		// 0 Ultrafast, 1 Fast
@@ -38,120 +41,150 @@ on my pc the bandwidth is about 7.41 GB/sec :D
 
 #define BUFF1 16
 
+#define rotr(x, n) (( x>>n  ) | (x<<((sizeof(x)<<3)-n)))
+
 #pragma GCC optimize ("unroll-loops")
+struct sched_attr
+{
+  uint32_t size;
+  uint32_t sched_policy;
+  uint64_t sched_flags;
+  int32_t sched_nice;
+  uint32_t sched_priority;
+  uint64_t sched_runtime;
+  uint64_t sched_deadline;
+  uint64_t sched_period;
+};
+unsigned int flags = 0;
+
+static int
+sched_setattr (pid_t pid, const struct sched_attr *attr, unsigned int flags)
+{
+  return syscall (SYS_sched_setattr, pid, attr, flags);
+}
+
+static inline int
+ioprio_set (int which, int who, int ioprio)
+{
+  return syscall (SYS_ioprio_set, which, who, ioprio);
+}
 
 int
 main (int argc, char **argv)
 {
-	unsigned int ncores = 0, nthreads = 0;
+  unsigned int ncores = 0, nthreads = 0;
 
-	asm volatile ("cpuid":"=a" (ncores), "=b" (nthreads):"a" (0xb), "c" (0x1):);
+  asm volatile ("cpuid":"=a" (ncores), "=b" (nthreads):"a" (0xb), "c" (0x1):);
 
-	long pipe_size = (long) fcntl (1, F_GETPIPE_SZ);
+  long pipe_size = (long) fcntl (1, F_GETPIPE_SZ);
 
-	if (pipe_size != -1)
+  if (pipe_size != -1)
+    {
+      fcntl (1, F_SETPIPE_SZ, pipe_size << 3);
+      pipe_size = (long) fcntl (1, F_GETPIPE_SZ);
+    }
+
+#ifdef __x86_64
+  long unsigned int init;
+  asm volatile ("rdtsc":"=a" (init):);
+#else
+  long unsigned int init = 0;
+  init = (long unsigned int) &init << 16 >> 24;
+#endif
+
+  if (ncores > 1)
+    fork ();
+  if (ncores > 3)
+    fork ();
+
+  struct sched_attr attr;
+  attr.size = sizeof (attr);
+  attr.sched_flags = 0;
+  attr.sched_nice = 99;
+  attr.sched_priority = 99;
+
+  attr.sched_policy = SCHED_RR;
+  attr.sched_runtime = 0;
+  attr.sched_period = attr.sched_deadline = 0;
+
+  sched_setattr (0, &attr, flags);	// chrt -r 99
+
+  ioprio_set (1, 0, (1 << 13) | 7);	// ionice -c 1 7
+  setpriority (0, 0, -20);	// nice -n -20
+
+
+  char buf[0x4000];
+  setvbuf (stdout, buf, _IOFBF, 0x4000);
+
+#ifdef __x86_64
+  long long unsigned int varr[BUFF1 + 2 + SLOWRND];
+#else
+  long unsigned int varr[BUFF1 + 2 + SLOWRND];
+#endif
+
+
+  if (argc > 1)
+    {
+      varr[BUFF1] = strtoull (argv[1], NULL, 16);
+      if (argc > 2)
+	varr[BUFF1 + 1] = strtoull (argv[2], NULL, 16);
+#if (SLOWRND >0)
+      if (argc > 3)
+	varr[BUFF1 + 2] = strtoull (argv[3], NULL, 16);
+#endif
+    }
+
+  if ((argc == 1) || ((varr[BUFF1] | varr[BUFF1 + 1]) == 0))
+    {
+
+      for (int x = 0; x < (2 + SLOWRND); x++)
 	{
-		fcntl (1, F_SETPIPE_SZ, pipe_size << 3);
-		pipe_size = (long) fcntl (1, F_GETPIPE_SZ);
+	  asm volatile ("rdrand %0\n":"=r" (varr[BUFF1 + x]):);
 	}
 
-	#define rotr(x, n) (( x>>n  ) | (x<<((sizeof(x)<<3)-n)))
-
-	#ifdef __x86_64
-	long unsigned int init;
-	asm volatile ("rdtsc":"=a" (init):);
-	#else
-	long unsigned int init = 0;
-	init = (long unsigned int) &init << 16 >> 24;
-	#endif
-
-	if (ncores > 1)
-	fork ();
-	if (ncores > 3)
-	fork ();
-
-	pthread_t this_thread = pthread_self ();
-
-	struct sched_param params;
-
-	params.sched_priority = sched_get_priority_max (SCHED_RR);
-
-	pthread_setschedparam (this_thread, SCHED_RR, &params);
-
-	char buf[0x4000];
-	setvbuf (stdout, buf, _IOFBF, 0x4000);
-
-	#ifdef __x86_64
-	long long unsigned int varr[BUFF1 + 2 + SLOWRND];
-	#else
-	long unsigned int varr[BUFF1 + 2 + SLOWRND];
-	#endif
+    }
 
 
-	if (argc > 1)
+  if (argc == 1)
+    {
+      for (int i = 0; i < (init && 31); i++)
 	{
-		varr[BUFF1] = strtoull (argv[1], NULL, 16);
-		if (argc > 2)
-		varr[BUFF1 + 1] = strtoull (argv[2], NULL, 16);
-		#if (SLOWRND >0)
-		if (argc > 3)
-		varr[BUFF1 + 2] = strtoull (argv[3], NULL, 16);
-		#endif
+	  varr[0] = varr[BUFF1] + varr[BUFF1 + 1];
+#if (ALGO==0)
+	  varr[0] ^= (varr[0] < varr[BUFF1]);
+#else
+	  varr[0] = rotr (varr[0], 8);
+#endif
+#if (SLOWRND > 0)
+	  varr[0] = varr[0] + varr[BUFF1 + 2];
+	  varr[0] ^= (varr[0] < varr[BUFF1 + 2]);
+	  varr[BUFF1 + 2] = varr[BUFF1 + 1];
+#endif
+	  varr[BUFF1 + 1] = varr[BUFF1];
+	  varr[BUFF1] = varr[0];
+	}
+    }
+
+  while (1)
+    {
+
+      for (int i = 0; i < BUFF1; i++)
+	{
+	  varr[i] = varr[BUFF1] + varr[BUFF1 + 1];
+#if (ALGO==0)
+	  varr[i] ^= (varr[i] < varr[BUFF1]);
+#else
+	  varr[i] = rotr (varr[i], 8);
+#endif
+#if (SLOWRND > 0)
+	  varr[i] = varr[i] + varr[BUFF1 + 2];
+	  varr[i] ^= (varr[i] < varr[BUFF1 + 2]);
+	  varr[BUFF1 + 2] = varr[BUFF1 + 1];
+#endif
+	  varr[BUFF1 + 1] = varr[BUFF1];
+	  varr[BUFF1] = varr[i];
 	}
 
-	if ((argc == 1) || ((varr[BUFF1] | varr[BUFF1 + 1]) == 0))
-	{
-		/* This gets the random seed from the cpu */
-
-		for (int x = 0; x < (2 + SLOWRND); x++)
-		{
-			asm volatile ("rdrand %0\n":"=r" (varr[BUFF1 + x]):);
-		}
-
-	}
-
-	/* This routine adds a random initialization */
-
-	if (argc == 1)
-	{
-		for (int i = 0; i < (init && 31); i++)
-		{
-			varr[0] = varr[BUFF1] + varr[BUFF1 + 1];
-			#if (ALGO==0)
-			varr[0] ^= (varr[0] < varr[BUFF1]);
-			#else
-			varr[0] = rotr (varr[0], 8);
-			#endif
-			#if (SLOWRND > 0)
-			varr[0] = varr[0] + varr[BUFF1 + 2];
-			varr[0] ^= (varr[0] < varr[BUFF1 + 2]);
-			varr[BUFF1 + 2] = varr[BUFF1 + 1];
-			#endif
-			varr[BUFF1 + 1] = varr[BUFF1];
-			varr[BUFF1] = varr[0];	// slower with _rotr64(varr[0], 8);
-		}
-	}
-
-	while (1)
-	{
-
-		for (int i = 0; i < BUFF1; i++)
-		{
-			varr[i] = varr[BUFF1] + varr[BUFF1 + 1];
-			#if (ALGO==0)
-			varr[i] ^= (varr[i] < varr[BUFF1]);
-			#else
-			varr[i] = rotr (varr[i], 8);
-			#endif
-			#if (SLOWRND > 0)
-			varr[i] = varr[i] + varr[BUFF1 + 2];
-			varr[i] ^= (varr[i] < varr[BUFF1 + 2]);
-			varr[BUFF1 + 2] = varr[BUFF1 + 1];
-			#endif
-			varr[BUFF1 + 1] = varr[BUFF1];
-			varr[BUFF1] = varr[i];	// slower with _rotr64(varr[i], 8);
-		}
-
-		fwrite (&varr, BUFF1 * sizeof (varr[0]), 1, stdout);
-	}
+      fwrite (&varr, BUFF1 * sizeof (varr[0]), 1, stdout);
+    }
 }
